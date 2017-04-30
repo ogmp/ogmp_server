253856f2a7460495a436665cf0b76ed7d93b454d
OGMP-Gyrth <gyrthmcmulin@gmail.com>
2017-04-30 17:15:50 +0200

    Removed the status types from the replies.

--- include/reply.hpp
+++ include/reply.hpp
@@ -12,26 +12,6 @@ namespace server {
 
 /// A reply to be sent to a client.
 struct reply {
-	/// The status of the reply.
-	enum status_type {
-		ok = 200,
-		created = 201,
-		accepted = 202,
-		no_content = 204,
-		multiple_choices = 300,
-		moved_permanently = 301,
-		moved_temporarily = 302,
-		not_modified = 304,
-		bad_request = 400,
-		unauthorized = 401,
-		forbidden = 403,
-		not_found = 404,
-		internal_server_error = 500,
-		not_implemented = 501,
-		bad_gateway = 502,
-		service_unavailable = 503
-	} status;
-
 	/// The headers to be included in the reply.
 	std::vector<header> headers;
 
@@ -53,8 +33,6 @@ struct reply {
 	vector<unsigned char> intToByteArray(int value);
 	void floatToByteArray(float f);
 	void add_size_byte();
-	/// Get a stock reply.
-	static reply stock_reply(status_type status);
 };
 
 } // namespace server
