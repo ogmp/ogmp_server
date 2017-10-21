a29cc84c680e1cee821e84d99381e01d8aba562a
ogmp-gyrth <gyrthmcmulin@gmail.com>
2017-10-21 17:40:02 +0200

    Fixed linux server start script.

--- misc/run_server.sh
+++ misc/run_server.sh
@@ -1,22 +1,24 @@
 #! /bin/sh
 ADDRESS=0.0.0.0
-PORT=80
-DIRECTORY=htdocs
+PORT=9000
+DIRECTORY=./
 
 if hash gnome-terminal 2>/dev/null; then
-    if (( $PORT < 1024 )); then
+    if [ $PORT -lt 1024 ]; then
+        echo "Sudo!"
         gnome-terminal --tab -e "/bin/bash -c 'sudo ./ogmp $ADDRESS $PORT $DIRECTORY; exec /bin/bash -i'" $ADDRESS $PORT $DIRECTORY
     else
+        echo "no sudo!"
         gnome-terminal --tab -e "/bin/bash -c './ogmp $ADDRESS $PORT $DIRECTORY; exec /bin/bash -i'" $ADDRESS $PORT $DIRECTORY
     fi
 elif hash xterm 2>/dev/null; then
-    if (( $PORT < 1024 )); then
+    if [ $PORT -lt 1024 ]; then
         sudo xterm -hold -e ./ogmp $ADDRESS $PORT $DIRECTORY
     else
         xterm -hold -e ./ogmp $ADDRESS $PORT $DIRECTORY
     fi
 elif hash konsole 2>/dev/null; then
-    if (( $PORT < 1024 )); then
+    if [ $PORT -lt 1024 ]; then
         sudo konsole --noclose -e ./ogmp $ADDRESS $PORT $DIRECTORY
     else
         konsole --noclose -e ./ogmp $ADDRESS $PORT $DIRECTORY
